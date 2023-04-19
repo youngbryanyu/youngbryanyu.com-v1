d2969061e24f49bdc432c2c0715f1538cbc5866b
Alan Chen <github@zenan.ch>
2023-04-18 19:23:22 -0500

    Add projects and ventures

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,8 +9,17 @@ import { ActivityType } from "components/Activity";
 
 import cuhkLogo from "public/schools/CUHK.png";
 import umnLogo from "public/schools/UMN.png";
+
 import githubLogo from "public/Github.png";
+import jupyterLogo from "public/projects/jupyter.png";
+import qianjianLogo from "public/ventures/qianjian.png";
+import stayLogo from "public/ventures/stay.jpeg";
+import openaiLogo from "public/projects/openai-logo.png";
+import nosediveLogo from "public/projects/nosedive.png";
+import canvasLogo from "public/projects/canvas.png";
+
 import avatar from "public/avatar.png";
+
 import { GetStaticProps } from "next";
 import { Project, allProjects } from "../.contentlayer/generated";
 import { pick } from "lib/pick";
@@ -46,11 +55,49 @@ const workplaces = [
 
 const sideProjects = [
   {
+    title: "ChatGPT Quick Actions for Raycast",
+    time: "2023",
+    description: "Invoke ChatGPT anywhere on your Mac",
+    imageSrc: openaiLogo,
+    link: "https://github.com/alanzchen/chatgpt-quick-actions",
+  },
+  {
+    title: "Canvas Tools",
+    time: "2022",
+    description: "A set of CLI tools for Canvas LMS",
+    imageSrc: canvasLogo,
+    link: "https://github.com/alanzchen/Canvas-Tools"
+  },
+  {
     title: "Jupyter Desktop",
     time: "2020",
     description: "macOS App for Jupyter Lab",
-    imageSrc: githubLogo,
+    imageSrc: jupyterLogo,
     link: "https://github.com/alanzchen/jupyter-desktop",
+  },
+  {
+    title: "Nosedive",
+    time: "2017",
+    description: "Parody website of Black Mirror's Nosedive",
+    imageSrc: nosediveLogo,
+    link: "https://github.com/alanzchen/nosedive/",
+  }
+];
+
+const ventures = [
+  {
+    title: "浅见 (Qianjian)",
+    time: "2014 - 2018",
+    description: "Online campus media for CUHK(SZ)",
+    imageSrc: qianjianLogo,
+    link: "https://qianjian.space",
+  },
+  {
+    title: "月台 (Stay)",
+    time: "2015 - 2017",
+    description: "Campus magazine for CUHK(SZ)",
+    imageSrc: stayLogo,
+    link: "https://qianjian.space",
   }
 ];
 
@@ -87,7 +134,7 @@ export default function About({ projects }: AboutProps) {
           />
         </div>
         <div
-          className="flex flex-col gap-16 animate-in sm:animate-none md:gap-24"
+          className="flex flex-col gap-16 animate-in sm:animate-none md:gap-16"
           style={{ "--index": 2 } as React.CSSProperties}
         >
           <Section heading="About me" headingAlignment="right">
@@ -98,32 +145,6 @@ export default function About({ projects }: AboutProps) {
               </p>
             </div>
           </Section>
-          <Section heading="Education" headingAlignment="right">
-            <div className="flex flex-col w-full gap-8">
-              <p>
-                My advisor is <Link href="https://carlsonschool.umn.edu/faculty/jason-chan">Jason Chan</Link> and my co-advisor is {" "}
-                <Link href={`https://carlsonschool.umn.edu/faculty/soumya-sen`}>Soumya Sen</Link> 
-              </p>
-              <Workplaces items={workplaces} />
-            </div>
-          </Section>
-          {/* <Section heading="Research" headingAlignment="right">
-              <ul className="flex flex-col gap-16">
-                {projects.map((project) => (
-                  <li key={project.title}>
-                      <div className="flex flex-col gap-5">
-                        <div className="flex flex-col gap-1">
-                          <h3>{project.title}</h3>
-                          <p className="text-secondary">{project.description}</p>
-                          <Link href={`/project/${project.slug}`} underline>
-                            Read More
-                          </Link>
-                        </div>
-                      </div>
-                  </li>
-                ))}
-              </ul>
-          </Section> */}
           <Section heading="Connect" headingAlignment="right">
             <ul className="flex gap-6 animated-list">
               {connectLinks.map((link) => (
@@ -133,10 +154,25 @@ export default function About({ projects }: AboutProps) {
               ))}
             </ul>
           </Section>
-          <Section heading="Side projects" headingAlignment="right">
+          <Section heading="Education" headingAlignment="right">
+            <div className="flex flex-col w-full gap-8">
+              <p>
+                My advisor is <Link href="https://carlsonschool.umn.edu/faculty/jason-chan">Jason Chan</Link> and my co-advisor is {" "}
+                <Link href={`https://carlsonschool.umn.edu/faculty/soumya-sen`}>Soumya Sen</Link> 
+              </p>
+              <Workplaces items={workplaces} />
+            </div>
+          </Section>
+          <Section heading="Side Projects" headingAlignment="right">
             <div className="flex flex-col w-full gap-8">
               <p>I am also a self-taught full-stack developer. I build stuff for fun :) </p>
               <Workplaces items={sideProjects} isAnimated />
+            </div>
+          </Section>
+          <Section heading="Past Ventures" headingAlignment="right">
+            <div className="flex flex-col w-full gap-8">
+              <p>Ventures I have founded, co-founded, or advised. </p>
+              <Workplaces items={ventures} isAnimated />
             </div>
           </Section>
         </div>
