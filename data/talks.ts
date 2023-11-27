1fe89631cda0f3a77a966f78b75e21b127e55ca1
Alan Chen <github@zenan.ch>
2023-11-27 00:01:11 -0600

    Use short dates

--- data/talks.ts
+++ data/talks.ts
@@ -3,41 +3,41 @@ export const talks = [
     conference: "University of Rochester",
     title: "Large Language Model In Creative Work",
     location: "Rochester, New York",
-    date: "December 11, 2023",
+    date: "Dec 11, 2023",
     invited: true
   },
   {
     conference: "Johns Hopkins University",
     title: "Large Language Model In Creative Work",
     location: "Baltimore, Maryland",
-    date: "December 5, 2023",
+    date: "Dec 5, 2023",
     invited: true
   },
   {
     conference: "Northeastern University",
     title: "Large Language Model In Creative Work",
     location: "Boston, Massachusetts",
-    date: "November 21, 2023",
+    date: "Nov 21, 2023",
     invited: true
   },
   {
     conference: "University of Texas at Dallas",
     title: "Large Language Model In Creative Work",
     location: "Dallas, Texas",
-    date: "November 29, 2023",
+    date: "Nov 29, 2023",
     invited: true
   },
   {
     conference: "Workshop on e-Business (WeB)",
     title: "Large Language Model In Creative Work",
     location: "Hyderabad, India",
-    date: "December 9, 2023"
+    date: "Dec 9, 2023"
   },
   {
     conference: "HKU Business School",
     title: "Large Language Model In Creative Work",
     location: "Hong Kong, China",
-    date: "November 2, 2023",
+    date: "Nov 2, 2023",
     link: "https://www.hkubs.hku.hk/event/how-large-language-model-in-creative-work-the-role-of-collaboration-modality-and-user-expertise/",
     invited: true
   },
@@ -45,7 +45,7 @@ export const talks = [
     conference: "CUHK Business School",
     title: "Large Language Model In Creative Work",
     location: "Hong Kong, China",
-    date: "October 30, 2023",
+    date: "Oct 30, 2023",
     link: "https://www.bschool.cuhk.edu.hk/events/large-language-model-in-creative-work-the-role-of-collaboration-modality-and-user-expertise/",
     invited: true
   },
@@ -53,132 +53,132 @@ export const talks = [
     conference: "Workshop on Information Systems and Economics (WISE)",
     title: "Large Language Model In Creative Work",
     location: "Hyderabad, India",
-    date: "December 13, 2023"
+    date: "Dec 13, 2023"
   },
   {
     conference: "Conference on Artificial Intelligence, Machine Learning, and Business Analytics",
     title: "Large Language Model In Creative Work",
     location: "Philadelphia, Pannsylvania",
-    date: "December 1, 2023"
+    date: "Dec 1, 2023"
   },
   {
     conference: "Conference on Digital Experimentation (CoDE) @ MIT",
     title: "Large Language Model In Creative Work",
     location: "Cambridge, Massachusetts",
-    date: "November 10, 2023"
+    date: "Nov 10, 2023"
   },
   {
     conference: "Conference in Information Systems and Technology (CIST)",
     title: "Large Language Model In Creative Work",
     award: "Best Student Paper Award Runner-up",
     location: "Phoenix, Arizona",
-    date: "October 14, 2023"
+    date: "Oct 14, 2023"
   },
   {
     conference: "INFORMS Annual Meeting",
     title: "Large Language Model In Creative Work",
     location: "Phoenix, Arizona",
-    date: "October 15, 2023"
+    date: "Oct 15, 2023"
   },
   {
     conference: "Business & Generative AI Workshop @ Wharton",
     title: "Large Language Model In Creative Work",
     location: "San Francisco, California",
-    date: "September 8, 2023"
+    date: "Sep 8, 2023"
   },
   {
     conference: "International Conference on Information Systems (ICIS)",
     title: "Less is Not Always More: Investigating the Impact of Block Intensity and Immediacy of Social Media Blockers on Productivity",
     award: "Best Paper in General IS Track",
     location: "Copenhagen, Denmark",
-    date: "December 13, 2022",
+    date: "Dec 13, 2022",
     link: "https://aisel.aisnet.org/icis2022/general_is/general_is/1/"
   },
   {
     conference: "Conference in Information Systems and Technology (CIST)",
     title: "Net Neutrality: Network Congestion, Social Welfare and Service Tiering Regulations",
     location: "Seattle, Washington",
-    date: "October 19, 2019",
+    date: "Oct 19, 2019",
   },
   {
     conference: "Conference in Information Systems and Technology (CIST)",
     title: "Investigating the Coordination of Functionality Richness, First-party Content Quality, and Revenue-sharing on Digital Platforms",
     location: "Virtual",
-    date: "November 8, 2020"
+    date: "Nov 8, 2020"
   },
   {
     conference: "Conference in Information Systems and Technology (CIST)",
     title: "Less is Not Always More: Investigating the Impact of Block Intensity and Immediacy of Social Media Blockers on Productivity",
     location: "Indianapolis, Indiana",
-    date: "October 16, 2022"
+    date: "Oct 16, 2022"
   },
   {
     conference: "Workshop on Information Technologies and Systems (WITS)",
     title: "Less is Not Always More: Investigating the Impact of Block Intensity and Immediacy of Social Media Blockers on Productivity",
     award: "Best Student Paper Award",
     location: "Copenhagen, Denmark",
-    date: "December 14, 2022",
+    date: "Dec 14, 2022",
     link: "https://witsconf.org/wits2022-awards/"
   },
   {
     conference: "Workshop on Information Technologies and Systems (WITS)",
     title: "Investigating the Coordination of Functionality Richness, First-party Content Quality, and Revenue-sharing on Digital Platforms",
     location: "Austin, Texas",
-    date: "December 16, 2021"
+    date: "Dec 16, 2021"
   },
   {
     conference: "Symposium on Statistical Challenges in Electronic Commerce Research (SCECR)",
     title: "Attendance Tracking Apps Academic Performance: Evidence from a Field Study",
     location: "Virtual",
-    date: "June 17, 2021"
+    date: "Jun 17, 2021"
   },
   {
     conference: "Symposium on Statistical Challenges in Electronic Commerce Research (SCECR)",
     title: "Less is Not Always More: Investigating the Impact of Block Intensity and Immediacy of Social Media Blockers on Productivity",
     location: "Madrid, Spain",
-    date: "June 21, 2022"
+    date: "Jun 21, 2022"
   },
   {
     conference: "Platform Strategy Symposium",
     title: "Investigating the Coordination of Functionality Richness, First-party Content Quality, and Revenue-sharing on Digital Platforms",
     location: "Boston, Massachusetts",
     link: "https://www.youtube.com/watch?v=iQK2ZKabHdI",
-    date: "July 19, 2022"
+    date: "Jul 19, 2022"
   },
   {
     conference: "Conference on Digital Experimentation (CoDE) @ MIT",
     title: "Less is Not Always More: Investigating the Impact of Block Intensity and Immediacy of Social Media Blockers on Productivity",
     location: "Cambridge, Massachusetts",
-    date: "October 21, 2022"
+    date: "Oct 21, 2022"
   },
   {
     conference: "Americas Conference on Information Systems (AMCIS)",
     title: "Less is Not Always More: Investigating the Impact of Block Intensity and Immediacy of Social Media Blockers on Productivity",
     location: "Minneapolis, Minnesota",
-    date: "August 13, 2022"
+    date: "Aug 13, 2022"
   },
   {
     conference: "INFORMS Annual Meeting",
     title: "Investigating the Coordination of Functionality Richness, First-party Content Quality , and Revenue-sharing on Digital Platforms",
     location: "Indianapolis, Indiana",
-    date: "October 18, 2022"
+    date: "Oct 18, 2022"
   },
   {
     conference: "China Workshop on Economics of Information Systems Theory (CWEIST)",
     title: "Dynamics of User Preference in Broadband Market",
     location: "Virtual",
-    date: "June 30, 2020"
+    date: "Jun 30, 2020"
   },
   {
     conference: "China Workshop on Economics of Information Systems Theory (CWEIST)",
     title: "Investigating the Coordination of Functionality Richness, First-party Content Quality, and Revenue-sharing on Digital Platforms",
     location: "Virtual",
-    date: "June 28, 2021"
+    date: "Jun 28, 2021"
   },
   {
     conference: "Industry Studies Association (ISA) Annual Conference",
     title: "Less is Not Always More: Investigating the Impact of Block Intensity and Immediacy of Social Media Blockers on Productivity",
     location: "Philadelphia, Pennsylvania",
-    date: "June 25, 2022"
+    date: "Jun 25, 2022"
   }
 ];
