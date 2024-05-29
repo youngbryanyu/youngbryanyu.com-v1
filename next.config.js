c2c68e93344c2b5a2e83667f13f2841dfbc1d6da
Young Yu <youngyu19@gmail.com>
2024-05-29 14:25:36 -0700

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,43 +2,52 @@ const { withContentlayer } = require("next-contentlayer"); // eslint-disable-lin
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "dgtzuqphqg23d.cloudfront.net", // TODO: remove?
-      },
-      { protocol: "https", hostname: "image.mux.com" },
-    ],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require("./scripts/generate-sitemap");
-      require("./scripts/generate-rss");
-    }
+    images: {
+        unoptimized: true,
+        // remotePatterns: [
+        //     {
+        //         protocol: "https",
+        //         hostname: "dgtzuqphqg23d.cloudfront.net", // TODO: remove?
+        //     },
+        //     { protocol: "https", hostname: "image.mux.com" },
+        // ],
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            require("./scripts/generate-sitemap");
+            require("./scripts/generate-rss");
+        }
 
-    // const fileLoaderRule = config.module.rules.find((rule) =>
-    //   rule.test?.test?.('.svg'),
-    // )
+        // const fileLoaderRule = config.module.rules.find((rule) =>
+        //   rule.test?.test?.('.svg'),
+        // )
 
-    // config.module.rules.push(
-    //   // Reapply the existing rule, but only for svg imports ending in ?url
-    //   {
-    //     ...fileLoaderRule,
-    //     test: /\.svg$/i,
-    //     resourceQuery: /url/, // *.svg?url
-    //   },
-    //   // Convert all other *.svg imports to React components
-    //   {
-    //     test: /\.svg$/i,
-    //     issuer: /\.[jt]sx?$/,
-    //     resourceQuery: { not: /url/ }, // exclude if *.svg?url
-    //     use: ['@svgr/webpack'],
-    //   },
-    // )
+        // config.module.rules.push(
+        //   // Reapply the existing rule, but only for svg imports ending in ?url
+        //   {
+        //     ...fileLoaderRule,
+        //     test: /\.svg$/i,
+        //     resourceQuery: /url/, // *.svg?url
+        //   },
+        //   // Convert all other *.svg imports to React components
+        //   {
+        //     test: /\.svg$/i,
+        //     issuer: /\.[jt]sx?$/,
+        //     resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        //     use: ['@svgr/webpack'],
+        //   },
+        // )
 
-    return config;
-  },
+        return config;
+    },
 };
 
 module.exports = withContentlayer(nextConfig);
+
+// module.exports = {
+//     images: {
+//         unoptimized: true,
+//     },
+//     output: 'export',
+//     distDir: 'out',
+// };
