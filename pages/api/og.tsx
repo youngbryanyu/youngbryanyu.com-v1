260030b8b04ffc614067543ed44dc061cba07ee2
Young Yu <youngyu19@gmail.com>
2024-05-29 23:42:36 -0700

    use Image over img

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,42 +1,51 @@
 import { ImageResponse } from '@vercel/og';
 import { NextRequest } from 'next/server';
-import { SiteURL } from '../about';
- 
+import { FullName, SiteURL } from '../about';
+import Image from 'next/image';
+
 export const config = {
-  runtime: 'edge',
+    runtime: 'edge',
 };
- 
+
 export default async function handler(request: NextRequest) {
-  const { searchParams } = request.nextUrl;
-  const title = searchParams.get('title');
- 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          display: 'flex',
-          fontSize: 60,
-          color: 'black',
-          background: '#f6f6f6',
-          width: '100%',
-          height: '100%',
-          paddingTop: 50,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <img
+    const { searchParams } = request.nextUrl;
+    const title = searchParams.get('title');
+
+    return new ImageResponse(
+        (
+            <div
+                style={{
+                    display: 'flex',
+                    fontSize: 60,
+                    color: 'black',
+                    background: '#f6f6f6',
+                    width: '100%',
+                    height: '100%',
+                    paddingTop: 50,
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                {/* <img
           width="256"
           height="256"
           src={`${SiteURL}/avatar.png`}
-        />
-        <p>{title}</p>
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    },
-  );
+        /> */}
+                <Image
+                    src={`${SiteURL}/avatar.png`}
+                    alt={`${FullName} avatar`}
+                    width={256}
+                    height={256}
+                    // className="w-8 h-8"
+                    priority
+                />
+                <p>{title}</p>
+            </div>
+        ),
+        {
+            width: 1200,
+            height: 630,
+        },
+    );
 }
